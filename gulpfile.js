79949e4fbc87501729003372a71533420a1daee7
Scofield001 <scofield-001@mail.ru>
2018-05-01 14:48:37 +0300

    Общие исправления

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require( 'gulp' ),
 	concat = require( 'gulp-concat' ),
 	sass = require( 'gulp-sass' ),
-	autoprefixer = require('gulp-autoprefixer'),
 	watch = require( 'gulp-watch' );
 
 gulp.task( 'sass', () => {
@@ -18,10 +17,6 @@ gulp.task( 'sass', () => {
 			.pipe( concat( 'style.scss' ))
 			.pipe( sass({
 				outputStyle: 'compressed'
-			}))
-			.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
 			}))
 			.pipe( gulp.dest( 'style/' ));
 
