500e276c58b141f041adf9569c6aa735a4918ef1
Scofield001 <scofield-001@mail.ru>
2018-05-11 17:01:53 +0300

    продолжаем делать корзину

--- js/Basket.js
+++ js/Basket.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Basket {
-    renderBasketGood() {
+    getItems() {
         let xhr = new XMLHttpRequest();
 
         xhr.open('GET', './responses/getBasket.json', true); //true - Асинхронный запрос
@@ -23,80 +23,105 @@ class Basket {
                 return;
             }
             let data = JSON.parse(xhr.responseText);
-
-            for (let i = 0; i < data.length; i++) {
-                let dataSet = data[i],
-                    list = document.getElementById('basket'),
-                    totalPrice = document.getElementById('totalPrice'),
-                    item = document.createElement('li'),
-                    link = document.createElement('a'),
-                    pic = document.createElement('img'),
-                    inner = document.createElement('div'),
-                    cap = document.createElement('h3'),
-                    stars = document.createElement('div'),
-                    text = document.createElement('span'),
-                    quantity = document.createElement('span'),
-                    multi = document.createElement('span'),
-                    price = document.createElement('span'),
-                    btnDel = document.createElement('button'),
-                    iDel = document.createElement('i'),
-                    count = document.getElementById('count');
-
-                item.classList.add('min-cart__item');
-                link.classList.add('min-cart__link');
-                link.setAttribute('href', 'good.html');
-                pic.classList.add('min-cart__img');
-                //Путь к данной картинке
-                pic.setAttribute('src', dataSet.img_url);
-                //Алтернативное название картинки
-                pic.setAttribute('alt', dataSet.product_name);
-                pic.setAttribute('width', '72');
-                inner.classList.add('min-cart__inner');
-                cap.classList.add('min-cart__title');
-                //Имя товара
-                cap.textContent = dataSet.product_name;
-                stars.classList.add('stars');
-                text.classList.add('min-cart__text');
-                quantity.classList.add('min-cart__quantity');
-                //Количество данного товара
-                quantity.textContent = dataSet.countGood.toString(); //Посчитать Заглушка
-                multi.classList.add('min-cart__multi');
-                multi.textContent = 'x';
-                price.classList.add('min-cart__price');
-                //Цена товра
-                price.textContent = '$' + dataSet.price;
-                btnDel.classList.add('btn-delete');
-                //id товара
-                btnDel.dataset.id = dataSet.id_product;
-                iDel.classList.add('fas', 'fa-times-circle');
-                //Общее количество товаров
-                count.textContent = data.length;
-                //Общая сумма товаров
-                totalPrice.textContent = '$300'; //Посчитать
-
-                list.appendChild(item).appendChild(link).appendChild(pic);
-                link.appendChild(inner).appendChild(cap);
-                inner.appendChild(price);
-                inner.appendChild(stars);
-                for (let k = 0; k < dataSet.rating; k++) { //Количество звезд (рейтинг)
-                    let star = document.createElement('span');
-                    star.classList.add('stars__icon');
-                    let iStar = document.createElement('i');
-                    iStar.classList.add('fas', 'fa-star', 'fa-xs');
-                    stars.appendChild(star).appendChild(iStar);
-                }
-                inner.appendChild(text).appendChild(quantity);
-                text.appendChild(multi);
-                text.appendChild(price);
-                item.appendChild(btnDel).appendChild(iDel);
-            }
+            basket.render(data);
         }
+    }
+    render(products) {
+        for (let i = 0; i < products.basket.length; i++) {
+            let list = document.getElementById('basket'),
+                totalPrice = document.getElementById('totalPrice'),
+                item = document.createElement('li'),
+                link = document.createElement('a'),
+                pic = document.createElement('img'),
+                inner = document.createElement('div'),
+                cap = document.createElement('h3'),
+                stars = document.createElement('div'),
+                text = document.createElement('span'),
+                quantity = document.createElement('span'),
+                multi = document.createElement('span'),
+                price = document.createElement('span'),
+                btnDel = document.createElement('button'),
+                iDel = document.createElement('i'),
+                count = document.getElementById('count');
+
+            item.classList.add('min-cart__item');
+            link.classList.add('min-cart__link');
+            link.setAttribute('href', 'good.html');
+            pic.classList.add('min-cart__img');
+            //Путь к данной картинке
+            pic.setAttribute('src', products.basket[i].img_url);
+            //Алтернативное название картинки
+            pic.setAttribute('alt', products.basket[i].product_name);
+            pic.setAttribute('width', '72');
+            inner.classList.add('min-cart__inner');
+            cap.classList.add('min-cart__title');
+            //Имя товара
+            cap.textContent = products.basket[i].product_name;
+            stars.classList.add('stars');
+            text.classList.add('min-cart__text');
+            quantity.classList.add('min-cart__quantity');
+            //Количество данного товара
+            quantity.textContent = '1'; //Посчитать Заглушка
+            multi.classList.add('min-cart__multi');
+            multi.textContent = 'x';
+            price.classList.add('min-cart__price');
+            //Цена товра
+            price.textContent = '$' + products.basket[i].price;
+            btnDel.classList.add('btn-delete');
+            //id товара
+            btnDel.dataset.id = products.basket[i].id_product;
+            iDel.classList.add('fas', 'fa-times-circle');
+            //Общее количество товаров
+            count.textContent = products.basket.length;
+            //Общая сумма товаров
+            totalPrice.textContent = '$300'; //Посчитать
+
+            list.appendChild(item).appendChild(link).appendChild(pic);
+            link.appendChild(inner).appendChild(cap);
+            inner.appendChild(price);
+            inner.appendChild(stars);
+            for (let k = 0; k < products.basket[i].rating; k++) { //Количество звезд (рейтинг)
+                let star = document.createElement('span');
+                star.classList.add('stars__icon');
+                let iStar = document.createElement('i');
+                iStar.classList.add('fas', 'fa-star', 'fa-xs');
+                stars.appendChild(star).appendChild(iStar);
+            }
+            inner.appendChild(text).appendChild(quantity);
+            text.appendChild(multi);
+            text.appendChild(price);
+            item.appendChild(btnDel).appendChild(iDel);
+        }
+    }
+    add(idProduct, name, price, path, pagePath, rating) {
+
+    this.getItems();
+
+        let basketItem = {
+            "id_product": idProduct,
+            "product_name": name,
+            "price": price,
+            "img_url": path,
+            "page_url": pagePath,
+            "rating": rating
+        };
+
+        data.amount += price;
+        data.basket.length++;
+        data.basket.push(basketItem);
     }
 }
 
 let basket = new Basket();
 
-basket.renderBasketGood();
-
-
+basket.getItems();
+basket.add('1', 'product two', '60', 'img/product-2.jpg', 'good.html', 3);
+basket.add('1', 'product two', '60', 'img/product-3.jpg', 'good.html', 3);
+basket.add('1', 'product two', '60', 'img/product-4.jpg', 'good.html', 3);
+/*      "id_product": 1,
+      "product_name": "product one",
+      "price": 50,
+      "img_url": "img/product-1.jpg",
+      "page_url": "good.html",
+      "rating": 5*/
 
